13e78cd005793feb607408a27394459b9c899a87
Michael Kosacki <michaelk@detournity.com>
2016-10-14 16:18:26 -0700

    Update urls and template for app

--- small_enterprise_tools/urls.py
+++ small_enterprise_tools/urls.py
@@ -18,8 +18,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+from django.conf.urls import url, include
+from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
+
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
-    # url(r'^api/', include('api.urls')),
+    url(r'^app/', csrf_exempt(TemplateView.as_view(template_name='index.html'))),
+    url(r'^api/', include('api.urls'))
 ]
